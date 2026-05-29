8e28d15cf6da16b7972d984f62511dd90090443d
nicolas <santos.moraes.nicolas@escola.pr.gov.br>
2026-05-28 23:08:15 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
+const modalidades = ["Ginastica", "Judo", "Surfe", "Volei"];
+
+//Com base na constante modalidade, 
+//Coloque o número que represente o esporte do seu grupo
+const escolha = 3; // 0=Ginástica, 1=Judô, 2=Surfe, 3=Vôlei
+
+document.querySelector('body').style.backgroundImage = "url('img/"+modalidades[escolha]+".png')";
+document.querySelector('title').textContent = "Missão Olímpica | "+modalidades[escolha];
+document.querySelector('h1').innerHTML = "Missão Olímpica <br> "+modalidades[escolha];
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+//Assim como a variável atual (acima)
+//Crie uma variável com o nome pontos que inicie com 0
+let pontos = 0;
+
+function mostraPergunta(){
+    if(atual >= perguntas[escolha].length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[escolha][atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + " ";
+    atual++;
+    pontos += opcaoSelecionada.pontos;
+    console.log(pontos);
+    mostraPergunta();
+}
+
+function mostraResultado(){
+    textoResultado.textContent = historiaFinal;
+    caixaPerguntas.textContent = "Resultado";
+    caixaAlternativas.textContent = "";
+    // chame a função podiumMedalhas aqui
+    podiumMedalhas();
+}
+
+//crie uma função podiumMedalhas
+function podiumMedalhas(){
+    if(pontos === 3){
+        caixaPrincipal.style.backgroundImage = "url('img/bronze.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 3 pontos é BRONZE!";
+    } else if(pontos === 4){
+        caixaPrincipal.style.backgroundImage = "url('img/prata.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 4 pontos é PRATA!";
+    } else if(pontos === 5){
+        caixaPrincipal.style.backgroundImage = "url('img/ouro.png')";
+        caixaPerguntas.textContent = "Resultado da competição: 5 pontos é OURO!";
+    } else {
+        caixaPrincipal.style.backgroundImage = "url('img/perdeu.png')";
+        caixaPerguntas.textContent = "Resultado da competição: PERDEU!";
+    }
+}
+
+mostraPergunta();
 
